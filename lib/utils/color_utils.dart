1afaaeab48f20d3f63587700b82b6b46d4583370
Aakash Rajbanshi <106716824+aakashx58@users.noreply.github.com>
2024-02-06 11:56:14 +0545

    first

---
+++ lib/utils/color_utils.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+  final secondaryColor=const Color(0xff111111);
+  final primaryColor=const Color(0xff7fff00);
+  final greyColor=Colors.grey;
+  final lightGreyColor=Colors.grey.shade300;
+  final cardColor=const Color(0xff222222);
+  final blackColor= Colors.black;
+  final whiteColor=Colors.white;
+
+}
